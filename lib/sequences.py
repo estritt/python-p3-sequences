d53de7be6edb2912e60a044cb1766be12ff1b0ee
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-02-21 09:22:36 -0500

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        seq = []
+    elif length == 1:
+        seq = [0]
+    else:
+        seq = [0, 1]
+    for i in range(2, length):
+        seq.append(seq[i-1] + seq[i-2])
+    print(seq)
